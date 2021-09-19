df98424fc2848ec4571b6062a3a47eab5f2d972b
sheikhemon <sheikhemon@gmail.com>
2021-09-19 19:16:04 +0600

    react-core-concept

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Hlw react </h2>
+        <h3>Welcome To react World</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start React
         </a>
       </header>
     </div>
